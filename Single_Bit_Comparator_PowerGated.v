b3310e9717a9ef194dcaaf35ad7f0fca2779bf72
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-01-16 11:11:56 +0530

    Create Single_Bit_Comparator_PowerGated.v

---
+++ Single_Bit_Comparator_PowerGated.v
@@ -0,0 +1,18 @@
+module Single_Bit_Comparator_PowerGated (
+    input enable, reset,
+    input a, b,
+    
+    output reg less_than, equal_to, greater_than    
+    );
+    
+    //This is a combinational circuit
+    //The enable line assists in power gating as this module will be duplicated in the top module
+    
+    always @ (enable) begin
+        #2;
+        if(reset) {less_than, equal_to, greater_than} = 3'b000;
+        else if(a^b) {less_than, equal_to, greater_than} = {a, 1'b0, b};
+        else {less_than, equal_to, greater_than} = 3'b010;
+    end
+    
+endmodule
