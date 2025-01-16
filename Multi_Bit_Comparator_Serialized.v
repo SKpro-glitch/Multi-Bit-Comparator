f97ad964731099f56278e2edc1ce093970549297
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-01-16 15:35:30 +0530

    Update Multi_Bit_Comparator_Serialized.v

--- Multi_Bit_Comparator_Serialized.v
+++ Multi_Bit_Comparator_Serialized.v
@@ -1,22 +1,23 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 
-module Multi_Bit_Comparator_Serialized #(parameter n=3) (    
+module Multi_Bit_Comparator #(parameter n=3) (    
     input clk, reset,
     input [n:0] a_in, b_in,
     
-    output reg less_than, equal_to, greater_than,
-    output reg [n:0] a, b, c
+    output reg less_than, equal_to, greater_than
     );
     
     //These registers are created to enable serialization
+    reg [n:0] a, b, c;
+    
+    //The start register notes when the comparision process is going on
     reg start=1;
     
     always @ (posedge clk) begin
         //The reset line will initialize all outputs to zero and the counter to all 1s
         if(reset) begin
             {less_than, equal_to, greater_than} = 3'b010;
-            c = {(n+1){1'b1}};
             start = 1'b1;
         end
         else if(start) begin
