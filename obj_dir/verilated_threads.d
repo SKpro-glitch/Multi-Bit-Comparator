7cae835a366144064e73981a283568f6e56cd49a
Soham Kapur <79128956+SKpro-glitch@users.noreply.github.com>
2025-06-13 09:30:57 +0530

    Delete obj_dir directory

--- obj_dir/verilated_threads.d
+++
@@ -1,8 +0,0 @@
-verilated_threads.o: \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilated_threads.cpp \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilatedos.h \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilated_threads.h \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilated.h \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilated_config.h \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilated_types.h \
- /tools/verilator/euvm-dev/5.031-dev/share/verilator/include/verilated_funcs.h
